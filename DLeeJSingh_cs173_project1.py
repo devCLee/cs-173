98e3416a9c611ad090c2f5c147f42095e5b2101f
Joel Singh <joelsingh788@gmail.com>
2025-01-27 02:41:51 -0500

    Remove trailing spaces on empty lines (format)

--- DLeeJSingh_cs173_project1.py
+++ DLeeJSingh_cs173_project1.py
@@ -4,10 +4,10 @@ January 27, 2025
 DLeeJSingh_cs173_project1.py
 This program simulates a situation where there are two people, and each person enters the kitchen starting at midnight, stays for a certain period, and then leaves for a specific amount of time, repeating this cycle throughout the day. At a specific time, starting at midnight, Soprano the cat attempts to hop up on the counter. The program determines whether Soprano succeeds if no one is in the kitchen or gets caught if someone is.
 """
-    
+
 def getParam():
     """Receives five inputs to simulate the situation and returns five integers.
-    
+
     Parameters:
         None
 
@@ -20,15 +20,15 @@ def getParam():
     """
     p1_in = int(input())
     p1_out = int(input())
-    
+
     p2_in = int(input())
     p2_out = int(input())
-    
+
     t_hop = int(input())
-    
+
     return p1_in, p1_out, p2_in, p2_out, t_hop
-    
-    
+
+
 def printCaught(p1_in, p1_out, p2_in, p2_out, t_hop):
     """Prints if Soprano the cat gets caught or not
 
@@ -38,7 +38,7 @@ def printCaught(p1_in, p1_out, p2_in, p2_out, t_hop):
         p2_in: length of time (in minutes) person 2 stays in the kitchen
         p2_out: length of time (in minutes) person 2 stays out of the kitchen
         t_hop: the time in minutes after midnight when Soprano attempts to hop
-        
+
     Return value:
         None
     """
@@ -49,7 +49,7 @@ def printCaught(p1_in, p1_out, p2_in, p2_out, t_hop):
             p_in: length of time (in minutes) the person stays in the kitchen
             p_out: length of time (in minutes) person stays out of the kitchen
             t_hop: the time in minutes after midnight when Soprano attempts to hop
-            
+
         Return value:
             A bool, whether or not Soprano was caught
         """
@@ -69,7 +69,7 @@ def printCaught(p1_in, p1_out, p2_in, p2_out, t_hop):
         print("CAUGHT")
     else:
         print("SUCCESS")
-        
+
 if __name__ == "__main__":
     p1_in, p1_out, p2_in, p2_out, t_hop = getParam()
     printCaught(p1_in, p1_out, p2_in, p2_out, t_hop)
